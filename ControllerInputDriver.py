b101b76617aa7e0c4662846847aadb5b190e40a3
Jason Crandall <jsonacrandall@gmail.com>
2023-09-26 00:55:57 -0600

    WIP controller support

--- ControllerInputDriver.py
+++ ControllerInputDriver.py
@@ -1,52 +1,97 @@
-from pydualsense import pydualsense, TriggerModes
-
-# def cross_pressed(state):
-#     print(state)
-
-# ds = pydualsense() # open controller
-# ds.init() # initialize controller
-
-# ds.cross_pressed += cross_pressed
-# ds.light.setColorI(255,0,0) # set touchpad color to red
-# ds.triggerL.setMode(TriggerModes.Rigid)
-# ds.triggerL.setForce(1, 255)
-# ds.close() # closing the controllerfrom Car import Car
+from pydualsense import pydualsense, TriggerModes, PlayerID
 
 class ControllerInput:
-
-    def __init__(self, car):
-        self.car = car
+    def __init__(self):
         self.ds = pydualsense()
+        self.controlSchemes = []
+        self.currentControlScheme = 0
+        self.bindingLookup = {
+            'dpad_down': self.ds.dpad_down,
+            'dpad_left': self.ds.dpad_left,
+            'dpad_right': self.ds.dpad_right,
+            'dpad_up': self.ds.dpad_up,
+            'left_stick': self.ds.left_joystick_changed,
+            'right_stick': self.ds.right_joystick_changed,
+            'r1': self.ds.r1_changed,
+            'r2': self.ds.r2_changed,
+            'r3': self.ds.r3_changed,
+            'l1': self.ds.l1_changed,
+            'l2': self.ds.l2_changed,
+            'l3': self.ds.l3_changed,
+            'cross': self.ds.cross_pressed,
+            'triangle': self.ds.triangle_pressed,
+            'circle': self.ds.circle_pressed,
+            'square': self.ds.square_pressed
+        }
 
-    def on_press(self, key):
-        if key.char == 'a':
-            print("Rotate left")
-            self.car.control_car(-self.car.power, self.car.power)
-        elif key.char == 'd':
-            print("Rotate right")
-            self.car.control_car(self.car.power, -self.car.power)
-        elif key.char == 'w':
-            print("Forward")
-            self.car.control_car(self.car.power, self.car.power)
-        elif key.char == 's':
-            print("Backwards")
-            self.car.control_car(-self.car.power, -self.car.power)
-
-    def on_release(self, key):
-        if key.char in "wasd":
-            self.car.control_car(0, 0)
-
-    def enable(self, isOn):
+    def enable(self, isOn = True):
         if isOn:
-            self.listener.start()
+            self.ds.init()
+            if len(self.controlSchemes) == 0:
+                print("ControllerInput should not be enabled without any control schemes")
+                return
+            self.set_control_scheme(self.currentControlScheme)
         else:
-            self.listener.stop()
+            self.ds.close()
 
+    def add_control_scheme(self, controlScheme):
+        self.controlSchemes.append(controlScheme)
 
-ds = pydualsense()
-ds.init()
-ds.triggerL.setMode(TriggerModes.Rigid)
-ds.triggerL.setForce(1, 255)
+    def set_control_scheme(self, index):
+        self._remove_bindings
 
-while True:
-    pass
+        print(f"Using control scheme {index}")
+
+        self.currentControlScheme = index
+        controlScheme = self.controlSchemes[index]
+        
+        # Update LED color
+        self.ds.light.setColorT(controlScheme.color)
+        self.ds.light.setPlayerID(PlayerID.PLAYER_1)
+
+        # Update trigger haptics        
+        # motorForce = max(0, min(controlScheme.motorForce, 255))        
+        motorForce = controlScheme.motorForce
+
+        if controlScheme.motorForce == 0:
+            self.ds.triggerL.setMode(TriggerModes.Off)
+            self.ds.triggerR.setMode(TriggerModes.Off)
+            self.ds.triggerL.setForce(1, 0)
+            self.ds.triggerR.setForce(1, 0)
+        else:
+            self.ds.triggerL.setMode(TriggerModes.Rigid)
+            self.ds.triggerR.setMode(TriggerModes.Rigid)
+            self.ds.triggerL.setForce(1, motorForce)
+            self.ds.triggerR.setForce(1, motorForce)
+
+        # Update bindings
+        self._add_bindings()
+
+    def toggle_control_scheme(self):
+        index = max(0, min(self.currentControlScheme + 1, len(self.controlSchemes) - 1))
+        self.set_control_scheme(index)
+
+    def _remove_bindings(self):
+        for bindingKey, bindingValue in self.controlSchemes[self.currentControlScheme].bindings.items():
+            if not bindingKey in self.bindingLookup:
+                continue
+            elif bindingKey == "triangle":
+                continue
+            self.bindingLookup[bindingKey] -= (bindingValue)
+
+    def _add_bindings(self):
+        for bindingKey, bindingValue in self.controlSchemes[self.currentControlScheme].bindings.items():
+            if not bindingKey in self.bindingLookup:
+                print(f"Invalid binding {bindingKey}")
+                continue
+            elif bindingKey == "triangle":
+                print(f"Triangle is reserved for changing control schemes")
+                continue
+            self.bindingLookup[bindingKey] += (bindingValue)
+            print(f"Binding {bindingKey}")
+
+    class ControlScheme:
+        def __init__(self, bindings, color = (0, 0, 255), motorForce = 255):
+            self.color = color
+            self.motorForce = motorForce
+            self.bindings = bindings
--- rasbotControl.py
+++ rasbotControl.py
@@ -1,23 +1,29 @@
 from KeyboardInputDriver import KeyboardInput
 from ControllerInputDriver import ControllerInput
-from Car import Car
+# from Car import Car
 
-def drive_forward():
-    car.control_car(car.power, car.power)
-def turn_left():
-    car.control_car(-car.power, car.power)
-def turn_right():
-    car.control_car(car.power, -car.power)
-def drive_backward():
-    car.control_car(-car.power, -car.power)
+# def drive_forward():
+#     car.control_car(car.power, car.power)
+# def turn_left():
+#     car.control_car(-car.power, car.power)
+# def turn_right():
+#     car.control_car(car.power, -car.power)
+# def drive_backward():
+#     car.control_car(-car.power, -car.power)
 
 
 if __name__ == '__main__':
-    car = Car()
-    keyboardInput = KeyboardInput()
-    keyboardInput.add_press_listener('w', drive_forward)
-    keyboardInput.add_press_listener('a', turn_left)
-    keyboardInput.add_press_listener('s', drive_backward)
-    keyboardInput.add_press_listener('d', turn_right)
+    # car = Car()
+    # keyboardInput = KeyboardInput()
+    # keyboardInput.add_press_listener('w', drive_forward)
+    # keyboardInput.add_press_listener('a', turn_left)
+    # keyboardInput.add_press_listener('s', drive_backward)
+    # keyboardInput.add_press_listener('d', turn_right)
 
     controllerInput = ControllerInput()
+    controllerInput.add_control_scheme(ControllerInput.ControlScheme(color=(0, 255, 0), motorForce=0, bindings={}))
+    controllerInput.add_control_scheme(ControllerInput.ControlScheme(color=(0, 255, 255), motorForce=255, bindings={}))
+    controllerInput.add_control_scheme(ControllerInput.ControlScheme(color=(255, 255, 0), motorForce=128, bindings={}))
+    controllerInput.enable()
+    while True:
+        pass
