2f1751a02564d0f8104022f9fbdb6d2b82450e74
Jason Crandall <jsonacrandall@gmail.com>
2023-09-29 20:42:26 -0600

    Update to keyboard package

--- KeyboardInputDriver.py
+++ KeyboardInputDriver.py
@@ -1,28 +1,23 @@
-from pynput import keyboard
+import keyboard
 
 class KeyboardInput:
 
     def __init__(self):
-        self.listener = keyboard.Listener(
-        on_press=self._on_press,
-        on_release=self._on_release)
+        keyboard.on_press(callback=self._on_press)
+        keyboard.on_release(callback=self._on_press)
         self.listeners = {}
+        self.enabled = False
 
     def _on_press(self, key):
-        if hasattr(key, "char") and key.char in self.listeners:
+        if self.enabled and key.name in self.listeners:
             self.listeners[key.char](True)
 
     def _on_release(self, key):
-        if hasattr(key, "char") and key.char in self.listeners:
-            self.listeners[key.char](False)
+        if self.enabled and key.name in self.listeners:
+            self.listeners[key.name](False)
 
     def add_listener(self, key, function):
         self.listeners[key] = function
 
     def enable(self, isOn = True):
-        if isOn:
-            self.listener.start()
-        else:
-            self.listener.stop()
-
-    # ...or, in a non-blocking fashion:
+        self.enabled = isOn
